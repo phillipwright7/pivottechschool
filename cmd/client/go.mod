c26f00d7f9abc5a4269c31ed4bf9a88a9cb40488
phillipwright7 <105671790+phillipwright7@users.noreply.github.com>
2022-10-29 21:23:25 -0500

    Refined product server, but difficulties with part B of acceptance criteria

---
+++ cmd/client/go.mod
@@ -0,0 +1,3 @@
+module github.com/phillipwright7/pivottechschool/cmd/client
+
+go 1.19
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
+
+const endpoint string = "http://localhost:8080/products"
 
 type product struct {
 	Name        string `json:"name"`
@@ -16,8 +17,8 @@ type product struct {
 	Price       int64  `json:"price"`
 }
 
-func productsGet() []product {
-	res, err := http.Get("http://localhost:8080/products")
+func allProducts() {
+	res, err := http.Get(endpoint)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,71 +33,55 @@ func productsGet() []product {
 	if err := json.Unmarshal(bs, &products); err != nil {
 		log.Fatal(err)
 	}
-	return products
-}
-
-func allProducts() {
-	products := productsGet()
 	for _, p := range products {
 		fmt.Printf("%d: %s: $%d\n", p.ID, p.Name, p.Price)
 	}
 }
 
 func oneProduct() {
-	var id int64
+	var id string
+	var product []product
+
 	fmt.Println("Which product ID would you like to return?")
 	fmt.Scanln(&id)
-	products := productsGet()
-	for i, p := range products {
-		if int64(i) == id {
-			fmt.Printf("%d: %s: $%d\n", p.ID, p.Name, p.Price)
-		}
-	}
-}
 
-func addProduct() {
-	var name, description string
-	var price int64
-
-	fmt.Println("Please enter the product name:")
-	fmt.Scanln(&name)
-	fmt.Println("Please enter the product description:")
-	fmt.Scanln(&description)
-	fmt.Println("Please enter the product price:")
-	fmt.Scanln(&price)
-
-	newProd := product{
-		Name:        name,
-		ID:          101,
-		Description: description,
-		Price:       price,
-	}
-
-	reqBody, err := json.Marshal(newProd)
-	res, err := http.Post("http://localhost:8080/products", "application/json", bytes.NewBuffer(reqBody))
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer res.Body.Close()
-	fmt.Printf("%s added to product server.", name)
+
+	req.Header.Set("id", id)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, &product)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(req)
 }
 
 func main() {
 	var input string
-	fmt.Println("Welcome to the product server. Please input the following numbers for whichever action you'd like to perform.")
-	fmt.Println("1: View entire list of products.")
-	fmt.Println("2: Return a specified product according to its ID.")
-	fmt.Println("3: Add a new product to the product server.")
-	fmt.Println("4: Update a product's name and price.")
-	fmt.Println("5: Remove a product from the server")
+	fmt.Printf("Welcome to the product server. Please input the following numbers for whichever action you'd like to perform.\n1: View entire list of products.\n2: Return a specified product according to its ID.\n3: Add a new product to the product server.\n4: Update a product's name and price.\n5: Remove a product from the server.\n")
 	fmt.Scanln(&input)
 	switch input {
 	case "1":
 		allProducts()
 	case "2":
 		oneProduct()
-	case "3":
-		addProduct()
 	default:
 		fmt.Println("Please input a valid number.")
 	}
