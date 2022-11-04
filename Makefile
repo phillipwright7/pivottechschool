58f5fe4eb5608a4bdbd4f3b715e0b1bee5f1d50a
phillipwright7 <105671790+phillipwright7@users.noreply.github.com>
2022-11-04 18:04:46 -0500

    Re-trying the product server assignment.

---
+++ Makefile
@@ -0,0 +1,17 @@
+run-server:
+	go run /Users/phillipwright/go/src/github.com/phillipwright7/pivottechschool/cmd/server/main.go
+
+get-products:
+	curl -i -X GET http://localhost:8080/products
+
+get-product:
+	curl -i -X GET http://localhost:8080/products/56
+
+add-product:
+	curl -i -X POST http://localhost:8080/products -d '{"name": "Apple", "description": "This is a test.", "price": 3}'
+
+update-product:
+	curl -i -X PUT http://localhost:8080/products/99 -d '{"name": "New iPhone", "description": "This is a new product.", "price": 749}'
+
+delete-product:
+	curl -i -X DELETE http://localhost:8080/products/99
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,144 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 
-	"github.com/phillipwright7/pivottechschool/cmd/calculator"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	a := 2
-	b := 2
-	sum := calculator.Add(a, b)
-	fmt.Println(sum)
-	dif := calculator.Subtract(a, b)
-	fmt.Println(dif)
-	pro := calculator.Multiply(a, b)
-	fmt.Println(pro)
-	quo, err := calculator.Divide(a, b)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(quo)
+type product struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+}
+
+var products []product
+
+func getProductsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(products); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+}
+
+func getProductHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Printf("error converting id to int: %v", err)
+	}
+	p := productFinder(id)
+	if p == nil {
+		log.Printf("product with id %d is not found", id)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(p); err != nil {
+		log.Printf("error encoding product: %v", id)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func addProductHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var p product
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		log.Printf("error decoding product: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	p.ID = len(products) + 1
+	products = append(products, p)
+	if err := json.NewEncoder(w).Encode(products); err != nil {
+		log.Printf("error encoding product: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func updateProductHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Printf("error converting id to int: %v", err)
+		return
+	}
+	p := productFinder(id)
+	if p == nil {
+		log.Printf("product with id %d is not found", id)
+		return
+	}
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		log.Printf("error decoding product: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	products[id] = *p
+	if err := json.NewEncoder(w).Encode(products); err != nil {
+		log.Printf("error encoding product: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func deleteProductHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Printf("error converting id to int: %v", err)
+		return
+	}
+	p := productFinder(id)
+	for i, prod := range products {
+		if prod.ID == p.ID {
+			products = append(products[:i], products[i+1:]...)
+		}
+	}
+	if err := json.NewEncoder(w).Encode(products); err != nil {
+		log.Printf("error encoding product: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func productFinder(id int) *product {
+	for _, p := range products {
+		if p.ID == id {
+			return &p
+		}
+	}
+	return nil
+}
+
+func initProducts() {
+	bs, err := os.ReadFile("cmd/server/products.json")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	if err := json.Unmarshal(bs, &products); err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func main() {
+	initProducts()
+	r := mux.NewRouter()
+	r.HandleFunc("/products", addProductHandler).Methods(http.MethodPost)
+	r.HandleFunc("/products/{id}", updateProductHandler).Methods(http.MethodPut)
+	r.HandleFunc("/products/{id}", deleteProductHandler).Methods(http.MethodDelete)
+	r.HandleFunc("/products/{id}", getProductHandler)
+	r.HandleFunc("/products", getProductsHandler)
+	log.Println("Listening on port :8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
